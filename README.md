4f89a8528d3b88933a44fe82652b61f604ce13cd
LiMingCong <2058615288@qq.com>
2025-11-02 03:22:54 +0800

    测试husky配置的prettier代码规范作用

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # vue3-Ts-admin-project-GuiGuZhenXuan
 
 ### 尚硅谷Vue项目实战硅谷甄选，vue3项目+TypeScript前端项目
-
-#### 项目介绍
--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 
 <template>
   <h1>Hello World</h1>
+
+  <h1>4544646</h1>
 </template>
 
 <style scoped></style>
