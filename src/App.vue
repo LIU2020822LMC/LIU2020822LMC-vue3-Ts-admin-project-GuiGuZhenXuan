94cabaa0d087cf7b73285a01536f46a812561231
LiMingCong <2058615288@qq.com>
2025-11-02 21:31:13 +0800

    feat: API接口统一管理

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,4 @@
-<script setup lang="ts">
-import { Login } from '@/api/user'
-import { onMounted } from 'vue'
-
-onMounted(() => {
-  Login({
-    username: 'system',
-    password: '111111',
-  })
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <h1>Svg图标测试</h1>
