acbbf5307d3aa9dc28e35f2ee7574663f7147dfd
LiMingCong <2058615288@qq.com>
2025-11-02 13:25:10 +0800

    feat: 配置stylelint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   // 校验规则
   rules: {
     'type-enum': [
-      2,
+      1,
       'always',
       [
         'feat',
@@ -18,12 +18,13 @@ module.exports = {
         'build',
       ],
     ],
-    'type-case': [0],
-    'type-empty': [0],
-    'scope-empty': [0],
-    'scope-case': [0],
-    'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never'],
-    'header-max-length': [0, 'always', 72],
+    'type-case': [1, 'always', 'lower-case'],
+    'type-empty': [1, 'never'],
+    'scope-empty': [1, 'never'],
+    'scope-case': [1, 'always', 'lower-case'],
+    'subject-empty': [1, 'never'],
+    'subject-full-stop': [1, 'never'],
+    'subject-case': [1, 'never'],
+    'header-max-length': [1, 'always', 72],
   },
 }
