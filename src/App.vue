16b636be06e022e9432a087b8e442640ea9b4e92
LiMingCong <2058615288@qq.com>
2025-11-02 13:47:29 +0800

    feat: 项目中element-plus集成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  Check,
+  Delete,
+  Edit,
+  Message,
+  Search,
+  Star,
+} from '@element-plus/icons-vue'
+</script>
 
 <template>
   <h1>Hello World</h1>
+  <el-button>Default</el-button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
+  <el-button type="warning">Warning</el-button>
+  <el-button type="danger">Danger</el-button>
+
+  <el-button :icon="Search" circle />
+  <el-button type="primary" :icon="Edit" circle />
+  <el-button type="success" :icon="Check" circle />
+  <el-button type="info" :icon="Message" circle />
+  <el-button type="warning" :icon="Star" circle />
+  <el-button type="danger" :icon="Delete" circle />
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.mount('#app')
